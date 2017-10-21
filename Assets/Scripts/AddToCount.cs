7e33fab72241d597c08bffa7d39b453cb8930a96
Codey Walker <codeyw@mtu.edu>
2017-10-20 23:18:40 -0400

    Merged game layout with character

---
+++ Assets/Scripts/AddToCount.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddToCount : MonoBehaviour {
+
+    public int score;
+    public int floor;
+    public Text scoreText;
+    public Text floorText;
+   
+    void Start () {
+        score = 0;
+        floor = 0;
+        SetScoreText();
+        setFloorText();
+	}
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            score = score + 10;
+            SetScoreText();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            floor = floor + 1;
+            setFloorText();
+        }
+
+     }
+
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void setFloorText()
+    {
+        floorText.text = "Floor: " + floor.ToString();
+    }
+}
