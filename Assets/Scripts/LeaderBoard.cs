58aed715a0c4f2fd8416f76545e3d959c4800782
ajstanle <ajstanle@mtu.edu>
2017-11-30 22:09:04 -0500

    added leaderboard and game win screens. Began leaderboard implementation

---
+++ Assets/Scripts/LeaderBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LeaderBoard : MonoBehaviour {
+
+    //initialize arrays to hold information parsed from text file
+    string[] names = new string[10];
+    int[] scores = new int[10];
+    double[] times = new double[10];
+
+    //Location of text file
+    string path = "Assets/Resources/leaderboard.txt";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
