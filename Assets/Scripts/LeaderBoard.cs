fbf85b5a06b3b8cae9ee5392edf87ca761a68c8d
ajstanle <ajstanle@mtu.edu>
2017-12-05 19:03:01 -0500

    began implementation of leaderboard

--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -5,17 +5,53 @@ using UnityEngine;
 
 public class LeaderBoard : MonoBehaviour {
 
+    public class Entry
+    {
+        public string name;
+        public int score;
+        public float time;
+    }
+
+    Entry[] leaderboard = new Entry[10];
+    
     //initialize arrays to hold information parsed from text file
     string[] names = new string[10];
     int[] scores = new int[10];
     double[] times = new double[10];
 
-    //Location of text file
-    string path = "Assets/Resources/leaderboard.txt";
+    // variables used to properly format the time when displayed on the leaderboard
+    public int milliseconds = 99;
+    public int minutes = 59;
+    public int seconds = 59;
+
+    StreamReader sR = new StreamReader("Assets/leaderboard.txt");
 
 	// Use this for initialization
 	void Start () {
-		
+        // read the file until there is nothing left to read
+		while (!sR.EndOfStream)
+        {
+            // fill teh leaderboard with the data from the text file
+            for (int i = 0; i < leaderboard.Length; i++)
+            {
+                leaderboard[i].name = sR.ReadLine();
+                leaderboard[i].score = int.Parse(sR.ReadLine());
+                leaderboard[i].time = float.Parse(sR.ReadLine());
+            }
+        }
+
+        sR.Close();
+
+        for (int i = 0; i < leaderboard.Length; i++)
+        {
+            names[i] = leaderboard[i].name;
+            scores[i] = leaderboard[i].score;
+
+            // determine minutes, seconds, milliseconds from time values
+            minutes = (int)leaderboard[i].time / 60;
+            seconds = (int)leaderboard[i].time % 60;
+        }
+
 	}
 	
 	// Update is called once per frame
