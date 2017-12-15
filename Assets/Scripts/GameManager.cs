353fa41ca4288d694cd31f6b4338140e0e71219f
Brandon Paupore <bjpaupor@mtu.edu>
2017-12-14 23:17:21 -0500

    Each floor has its own song

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,9 @@ public class GameManager : MonoBehaviour {
     public BuildFloor floorScript;
     public BuildRoom boardScript;
     public AudioSource song;
+    public AudioClip clip1;
+    public AudioClip clip2;
+    public AudioClip clip3;
     public GameObject cam;
     public int roomLength;
     public int numFloors;
@@ -37,6 +40,7 @@ public class GameManager : MonoBehaviour {
      * -Could be hard coded or random
      */
     void buildFinalFloor() {
+        song.Stop();
         BuildFloor.Room[,] floor = floorScript.buildFinalFloor(roomLength);
         for (int i = 0; i < floorScript.lengthOfFloor; i++)
             for (int j = 0; j < floorScript.heightOfFloor; j++)
@@ -50,6 +54,9 @@ public class GameManager : MonoBehaviour {
         floorScript = GetComponent<BuildFloor>();
         boardScript = GetComponent<BuildRoom>();
         song = GetComponent<AudioSource>();
+        clip3 = Resources.Load("Music/Descension3") as AudioClip;
+        clip2 = Resources.Load("Music/Descension2") as AudioClip;
+        clip1 = Resources.Load("Music/Descension1") as AudioClip;
         song.Play();
         objects = new List<List<GameObject>>();
         buildFloor();
@@ -61,11 +68,22 @@ public class GameManager : MonoBehaviour {
      */
     void buildFloor() {
         BuildFloor.Room[,] floor = floorScript.buildFloor(roomLength);
+        BuildFloor.FloorColor color = BuildFloor.FloorColor.GREY;
+        song.Stop();
         for (int i = 0; i < floorScript.lengthOfFloor; i++)
             for (int j = 0; j < floorScript.heightOfFloor; j++)
                 if (floor[i, j] != null) {
                     boardScript.SetupScene(roomLength, floor[i, j]);
                     objects.Add(boardScript.getList());
+                    if (color == BuildFloor.FloorColor.GREY)
+                        color = floor[i, j].color;
                 }
+        if (color == BuildFloor.FloorColor.BLUE)
+            song.clip = clip1;
+        else if (color == BuildFloor.FloorColor.PURPLE)
+            song.clip = clip2;
+        else if (color == BuildFloor.FloorColor.RED)
+            song.clip = clip3;
+        song.Play();
     }
 }
