6f7af7e0f0c4d1858358187d7b4ad2dcb7a4c7db
unknown <cganders@mtu.edu>
2018-02-12 18:47:55 -0500

    Added health bar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,122 +1,122 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿			using System.Collections;
+			using System.Collections.Generic;
+			using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+			public class Enemy : MonoBehaviour {
 
-	protected float speedMax;
-	public float speed = 2f;
+				protected float speedMax;
+				public float speed = 2f;
 
-	protected float xMax;
-	protected float yMax;
-	protected float xMin;
-	protected float yMin;
+				protected float xMax;
+				protected float yMax;
+				protected float xMin;
+				protected float yMin;
 
-	protected float x;
-	protected float y;
-	protected float time;
-	protected float angle;
+				protected float x;
+				protected float y;
+				protected float time;
+				protected float angle;
 
-	protected int health;
+				protected int health;
 
-	protected Rigidbody2D rb2d;
-	protected CircleCollider2D circleCollider;
-	protected GameObject player;
-	protected Transform player_pos;
+				protected Rigidbody2D rb2d;
+				protected CircleCollider2D circleCollider;
+				protected GameObject player;
+				protected Transform player_pos;
 
-    protected AudioSource audioSource;
-    public AudioClip deathSound;
+			    protected AudioSource audioSource;
+			    public AudioClip deathSound;
 
-    // Use this for initialization
-    protected virtual void Start () {
-		rb2d = GetComponent<Rigidbody2D> ();
-		circleCollider = GetComponent<CircleCollider2D> ();
+			    // Use this for initialization
+			    protected virtual void Start () {
+					rb2d = GetComponent<Rigidbody2D> ();
+					circleCollider = GetComponent<CircleCollider2D> ();
 
-		health = 50;
+					health = 50;
 
-//		Vector3 screenMax = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, Camera.main.nearClipPlane));
-//		Vector3 screenMin = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, Camera.main.nearClipPlane));
-
-
-		xMax = transform.position.x + 10;
-		xMin = transform.position.x - 10;
-		yMax = transform.position.y + 10;
-		yMin = transform.position.y - 10;
-		speedMax = speed / 30f;
-
-		x = Random.Range(-speedMax, speedMax);
-		y = Random.Range(-speedMax, speedMax);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
-		if (player == null)
-			MoveAtRandom ();
-		else {
-			Chase ();	
-		}
-
-		if (health < 0) {
-			Destroy(gameObject);
-            audioSource.PlayOneShot(deathSound);
-            print ("RIP");
-		}
-	}
-
-	protected virtual void MoveAtRandom() {
-		time += Time.deltaTime;
-
-		if (transform.localPosition.x > xMax) {
-			x = Random.Range(-speedMax, 0.0f);
-			time = 0.0f; 
-		}
-		if (transform.localPosition.x < xMin) {
-			x = Random.Range(0.0f, speedMax);
-			time = 0.0f; 
-		}
-		if (transform.localPosition.y > yMax) {
-			y = Random.Range(-speedMax, 0.0f);
-			time = 0.0f; 
-		}
-		if (transform.localPosition.y < yMin) {
-			y = Random.Range(0.0f, speedMax);
-			time = 0.0f; 
-		}
-
-		angle = Mathf.Atan2 (y, x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler (0, 0, angle-90);
-
-		if (time > 1.0f) {
-			x = Random.Range(-speedMax, speedMax);
-			y = Random.Range(-speedMax, speedMax);
-			time = 0.0f;
-		}
-
-		transform.localPosition = new Vector2(transform.localPosition.x + x, transform.localPosition.y + y);
-	}
-
-	void OnTriggerEnter2D (Collider2D other) {
-
-		if (other.gameObject.CompareTag ("Player")) {
-			
-			player = other.gameObject;
-			player_pos = player.GetComponent<Transform> ();
-		}
-	}
-
-	protected virtual void Chase() {
+			//		Vector3 screenMax = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, Camera.main.nearClipPlane));
+			//		Vector3 screenMin = Camera.main.ScreenToWorldPoint (new Vector3 (0, 0, Camera.main.nearClipPlane));
 
 
-		transform.position = Vector2.MoveTowards(transform.position, player_pos.position, speed * Time.deltaTime);
+					xMax = transform.position.x + 10;
+					xMin = transform.position.x - 10;
+					yMax = transform.position.y + 10;
+					yMin = transform.position.y - 10;
+					speedMax = speed / 30f;
 
-		angle = Mathf.Atan2 (player_pos.position.y - transform.position.y, player_pos.position.x - transform.position.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler (0, 0, angle);
+					x = Random.Range(-speedMax, speedMax);
+					y = Random.Range(-speedMax, speedMax);
+				}
 
-	}
+				// Update is called once per frame
+				void Update () {
 
-	void Hit(int dmg)
-	{
-		health -= dmg;
-	}
-}
+					if (player == null)
+						MoveAtRandom ();
+					else {
+						Chase ();	
+					}
+
+					if (health < 0) {
+						Destroy(gameObject);
+			            audioSource.PlayOneShot(deathSound);
+			            print ("RIP");
+					}
+				}
+
+				protected virtual void MoveAtRandom() {
+					time += Time.deltaTime;
+
+					if (transform.localPosition.x > xMax) {
+						x = Random.Range(-speedMax, 0.0f);
+						time = 0.0f; 
+					}
+					if (transform.localPosition.x < xMin) {
+						x = Random.Range(0.0f, speedMax);
+						time = 0.0f; 
+					}
+					if (transform.localPosition.y > yMax) {
+						y = Random.Range(-speedMax, 0.0f);
+						time = 0.0f; 
+					}
+					if (transform.localPosition.y < yMin) {
+						y = Random.Range(0.0f, speedMax);
+						time = 0.0f; 
+					}
+
+					angle = Mathf.Atan2 (y, x) * Mathf.Rad2Deg;
+					transform.rotation = Quaternion.Euler (0, 0, angle-90);
+
+					if (time > 1.0f) {
+						x = Random.Range(-speedMax, speedMax);
+						y = Random.Range(-speedMax, speedMax);
+						time = 0.0f;
+					}
+
+					transform.localPosition = new Vector2(transform.localPosition.x + x, transform.localPosition.y + y);
+				}
+
+				void OnTriggerEnter2D (Collider2D other) {
+
+					if (other.gameObject.CompareTag ("Player")) {
+						
+						player = other.gameObject;
+						player_pos = player.GetComponent<Transform> ();
+					}
+				}
+
+				protected virtual void Chase() {
+
+
+					transform.position = Vector2.MoveTowards(transform.position, player_pos.position, speed * Time.deltaTime);
+
+					angle = Mathf.Atan2 (player_pos.position.y - transform.position.y, player_pos.position.x - transform.position.x) * Mathf.Rad2Deg;
+					transform.rotation = Quaternion.Euler (0, 0, angle);
+
+				}
+
+				void Hit(int dmg)
+				{
+					health -= dmg;
+				}
+			}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	public float health; //current progress
+	public Slider healthSlider;
+	public PlayerController player;
+
+	void Start() {
+		health = 100;
+	}
+
+	void Update() {
+		healthSlider.value = player.health;
+	}
+
+}
