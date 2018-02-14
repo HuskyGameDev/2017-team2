365e0e5189e3c32751918e45db7027fd0afa380e
unknown <cganders@mtu.edu>
2018-02-14 18:31:32 -0500

    Health bar changes color

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,6 +8,7 @@ public class HealthBar : MonoBehaviour {
 	public float health; //current progress
 	public Slider healthSlider;
 	public PlayerController player;
+	public Image fill;
 
 	void Start() {
 		health = 100;
@@ -15,6 +16,17 @@ public class HealthBar : MonoBehaviour {
 
 	void Update() {
 		healthSlider.value = player.health;
+
+		if (healthSlider.value <= 50 && healthSlider.value > 15) {
+			fill.color = Color.yellow;
+		} else if (healthSlider.value > 50) {
+			fill.color = Color.green;
+		} else {
+			fill.color = Color.red;
+		}
+
+
+
 	}
 
 }
