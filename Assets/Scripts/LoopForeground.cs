82599e5b7fab3b40fe34865ffd83d62559486d0b
ajstanle <ajstanle@mtu.edu>
2018-02-28 18:38:06 -0500

    Fully implemented main menu background

---
+++ Assets/Scripts/LoopForeground.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopForeground : MonoBehaviour {
+
+    int loopHeight = 2400;
+    int buildingHeight = 3456;
+        	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 50 * Time.deltaTime, 0);
+
+        if (transform.position.y > loopHeight)
+        {
+            Vector2 offset = new Vector2(0, buildingHeight);
+            transform.position = (Vector2)transform.position - offset;
+        }
+    }
+}
---
+++ Assets/Scripts/slideBackground.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slideBackground : MonoBehaviour {
+
+    float savedTime;
+
+    // Use this for initialization
+    void Start () {
+        savedTime = Time.time;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time - savedTime <= 7)
+        {
+            transform.Translate(10 * Time.deltaTime, 0, 0);
+        }
+        else if (Time.time - savedTime >= 5 && Time.time - savedTime <= 14)
+        {
+            transform.Translate(-10 * Time.deltaTime, 0, 0);
+        }
+        else if (Time.time - savedTime > 14)
+        {
+            savedTime = Time.time;
+        }
+    }
+}
