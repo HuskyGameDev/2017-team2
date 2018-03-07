423be7f2deb15a71ae3ff8d36d399b7bf8ab1e96
ajstanle <ajstanle@mtu.edu>
2018-03-07 18:38:09 -0500

    implemented credits, resumed leaderboard

--- Assets/Scripts/LeaderBoard.cs
+++ Assets/Scripts/LeaderBoard.cs
@@ -37,15 +37,14 @@ public class LeaderBoard : MonoBehaviour {
 
     // variables that store the passed in values of the player's final score and time
     public string newName = "test";
-    public int newScore;
-    public float newTime;
+    public int newScore = 17500;
+    public float newTime = 277;
 
     // location of leaderboard text file
     static private string path = "Assets/leaderboard.txt";
 
     // initialize Stream Reader and StreamWriter
     StreamReader sR = new StreamReader(path);
-    StreamWriter sW = new StreamWriter(path);
 
 	// Use this for initialization
 	void Start () {
@@ -65,34 +64,13 @@ public class LeaderBoard : MonoBehaviour {
 
         sR.Close();
 
-        // format times so that they can be read more easily on leaderboard
-        for (int i = 0; i < leaderboard.Length; i++)
-        {
-            // determine minutes, seconds, milliseconds from time values
-            minutes = (int)leaderboard[i].Time / 60;
-            seconds = (int)leaderboard[i].Time % 60;
-            mSeconds = (int)leaderboard[i].Time % 1;
-
-            formatTime[i] = System.String.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, mSeconds);
-        }
-        
-
-        // fill the textboxes on the leaderboard with the data in the virtual leaderboard
-        for (int i = 0; i < leaderboard.Length; i++)
-        {
-            names[i].text = leaderboard[i].Name;
-            scores[i].text = leaderboard[i].Score.ToString();
-            times[i].text = formatTime[i];
-        }
-
         // get name from InputField and create a new Entry
         Entry playerEntry = new Entry(newName, newScore, newTime);
 
         // check to see if the new entry's score is high enough to go on the leaderboared
         if (playerEntry.Score < leaderboard[9].Score)
         {
-            // score wasn't high enough, go to leaderboard without saving score
-            // (do nothing)
+            
         } else {
             // player's score was high enough to go on leaderboard
             // overwrite last Entry with new Entry
@@ -126,7 +104,28 @@ public class LeaderBoard : MonoBehaviour {
             }
         }
 
-        /*
+        // format times so that they can be read more easily on leaderboard
+        for (int i = 0; i < leaderboard.Length; i++)
+        {
+            // determine minutes, seconds, milliseconds from time values
+            minutes = (int)leaderboard[i].Time / 60;
+            seconds = (int)leaderboard[i].Time % 60;
+            mSeconds = (int)leaderboard[i].Time % 1;
+
+            formatTime[i] = System.String.Format("{0:00}:{1:00}.{2:000}", minutes, seconds, mSeconds);
+        }
+
+        // fill the textboxes on the leaderboard with the data in the virtual leaderboard
+        for (int i = 0; i < leaderboard.Length; i++)
+        {
+            names[i].text = leaderboard[i].Name;
+            scores[i].text = leaderboard[i].Score.ToString();
+            times[i].text = formatTime[i];
+        }
+
+        StreamWriter sW = new StreamWriter(path);
+
+        
         // begin writing new data to the text file
         for (int i = 0; i < leaderboard.Length; i++)
         {
@@ -134,8 +133,8 @@ public class LeaderBoard : MonoBehaviour {
             sW.WriteLine(leaderboard[i].Score.ToString());
             sW.WriteLine(leaderboard[i].Time.ToString());
         }
+        
 
         sW.Close();
-        */
-	}
+    }
 }
---
+++ Assets/Scripts/ScrollingCredits.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingCredits : MonoBehaviour {
+
+    public Text thanks;
+    public Text msg1;
+    public Text msg2;
+    public Text msg3;
+    public Text msg4;
+
+    public Button menuButton;
+
+    float start;
+    
+    // Use this for initialization
+    void Start () {
+        start = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time - start >= 3)
+        {
+            thanks.transform.Translate(0, 50 * Time.deltaTime, 0);
+            msg1.transform.Translate(0, 50 * Time.deltaTime, 0);
+        }
+
+        if (Time.time - start >= 6)
+        {
+            msg2.transform.Translate(0, 50 * Time.deltaTime, 0);
+        }
+
+        if (Time.time - start >= 9)
+        {
+            msg3.transform.Translate(0, 50 * Time.deltaTime, 0);
+        }
+
+        if (Time.time - start >= 13)
+        {
+            msg4.transform.Translate(0, 50 * Time.deltaTime, 0);
+        }
+
+        if (thanks.transform.position.y > 800)
+        {
+            thanks.transform.position = new Vector3(0, 850, 0);
+            thanks.gameObject.SetActive(false);
+        }
+
+        if (msg1.transform.position.y > 800)
+        {
+            msg1.transform.position = new Vector3(0, 850, 0);
+            msg1.gameObject.SetActive(false);
+        }
+
+        if (msg2.transform.position.y > 800)
+        {
+            msg2.transform.position = new Vector3(0, 850, 0);
+            msg2.gameObject.SetActive(false);
+        }
+
+        if (msg3.transform.position.y > 800)
+        {
+            msg3.transform.position = new Vector3(0, 850, 0);
+            msg3.gameObject.SetActive(false);
+        }
+
+        if (msg4.transform.position.y > 850)
+        {
+            msg4.transform.position = new Vector3(0, 850, 0);
+            msg4.gameObject.SetActive(false);
+        }
+
+        if (Time.time >= 30)
+        {
+            menuButton.gameObject.SetActive(true);
+        }
+    }
+}
