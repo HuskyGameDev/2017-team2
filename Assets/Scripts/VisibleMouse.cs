0149ca63f7acb5f3bf3100eac67c9ed0d11dc889
ajstanle <ajstanle@mtu.edu>
2018-04-23 18:04:34 -0400

    began fixing cursor and controller related-bugs

---
+++ Assets/Scripts/VisibleMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisibleMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (DataBetweenScenes.gamePad == true)
+        {
+            Cursor.visible = false;
+        } else
+        {
+            Cursor.visible = true;
+        }
+
+        
+	}
+}
