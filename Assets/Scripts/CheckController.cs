7d24552dd4cee470037ec3d7f9ff8dfc4d202d46
ajstanle <ajstanle@mtu.edu>
2018-04-25 18:52:35 -0400

    mouse is visible on all end game screens, controller check might work

---
+++ Assets/Scripts/CheckController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckController : MonoBehaviour {
+
+    // string array used to see if there is currently a controller plugged in
+    private string[] controllers;
+
+    // float to determine when to check if there is a controller connected or not
+    private int checkControl = 180;
+
+    void Start() {
+        controllers = Input.GetJoystickNames();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Check for controller in update by counting the number of frames
+        checkControl++;
+
+        if (checkControl >= 180)
+        {
+            // update the Joystick Names array
+            controllers = Input.GetJoystickNames();
+            print(controllers.Length);
+            if (controllers.Length > 0)
+            {
+                print(controllers[0]);
+                if (!string.IsNullOrEmpty(controllers[0]))
+                {
+                    print("controller");
+                    DataBetweenScenes.gamePad = true;
+                    Cursor.visible = false;
+                }
+                else
+                {
+                    print("no controller");
+                    DataBetweenScenes.gamePad = false;
+                    Cursor.visible = true;
+                }
+            }
+
+            checkControl = 0;
+        }
+
+    }
+}
